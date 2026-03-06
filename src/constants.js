a7ca3927ef5e979f09f5650135ae4f068c2bdd2a
Teerth554 <132830420+Teerth554@users.noreply.github.com>
2026-03-06 09:34:55 +0530

    commit all

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const DB_NAME = "teerth710_db";
---
+++ src/index.js
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";    
+import {DB_NAME} from "../src/constants.js";
+import connectDB from "../src/db/index.js";
+
+dotenv.config({
+    path:'./.env'
+})
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app = express()
+// ( async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",()=>{
+//             console.log("Error :",error);
+//             throw
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         })
+//     }catch (error) {
+//         console.log("ERROR:",error);
+//         throw error;
+//     }
+// })()
